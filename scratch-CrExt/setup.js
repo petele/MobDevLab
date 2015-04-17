6331e6240c14e07edd7e7442366251f82c7340cf
Pete LePage <pete.lepage@pobox.com>
2015-04-17 15:14:15 -0400

    update copyright

--- scratch-CrExt/setup.js
+++ scratch-CrExt/setup.js
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  *
- * Mini Mobile Device Lab
+ * Mini Mobile Device Lab (Chrome Extension)
  * https://github.com/GoogleChrome/MiniMobileDeviceLab
  * setup.js
  *
